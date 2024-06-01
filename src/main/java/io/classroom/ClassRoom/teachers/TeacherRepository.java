06ad27d12aba09e22de8800ee7b919b6b43f15bc
Satyam Gupta <satyamgupta3803@gmail.com>
2024-06-01 14:41:12 +0530

    connects the interface to spring jpa for both the classes

---
+++ src/main/java/io/classroom/ClassRoom/teachers/TeacherRepository.java
@@ -0,0 +1,8 @@
+package io.classroom.ClassRoom.teachers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
