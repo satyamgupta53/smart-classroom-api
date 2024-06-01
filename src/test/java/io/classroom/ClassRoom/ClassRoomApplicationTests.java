be0f697553c03a269f5f32360a2aa773af6c9e9e
Satyam Gupta <satyamgupta3803@gmail.com>
2024-06-01 14:04:35 +0530

    initial commit for the project

---
+++ src/test/java/io/classroom/ClassRoom/ClassRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package io.classroom.ClassRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
