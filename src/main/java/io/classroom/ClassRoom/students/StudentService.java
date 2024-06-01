2c4ecf8a0856d1e51d0e914c642af553b9125d63
Satyam Gupta <satyamgupta3803@gmail.com>
2024-06-01 14:41:28 +0530

    hold the business logic for both the classes

---
+++ src/main/java/io/classroom/ClassRoom/students/StudentService.java
@@ -0,0 +1,20 @@
+package io.classroom.ClassRoom.students;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getstudent() {
+        return studentRepository.findAll();
+    }
+}
