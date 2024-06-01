e37041b8393c8bee0d335a0d41e5502e74346aa3
Satyam Gupta <satyamgupta3803@gmail.com>
2024-06-01 14:40:38 +0530

    hold the controller layer for both the classes

---
+++ src/main/java/io/classroom/ClassRoom/teachers/TeacherController.java
@@ -0,0 +1,26 @@
+package io.classroom.ClassRoom.teachers;
+
+import io.classroom.ClassRoom.students.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/teachers")
+public class TeacherController {
+    private final TeacherService teacherService;
+
+    @Autowired
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @GetMapping
+    public List<Teacher> getTeachers() {
+        return teacherService.getTeachers();
+    }
+
+}
